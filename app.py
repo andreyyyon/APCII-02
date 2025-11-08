ae8ef9f5684cb1adc8c95798f2c375d5d35eb912
andrey.rebelatto <lzzgrip@gmail.com>
2025-11-08 13:55:43 -0300

    cmt-08112025

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
+# GitHub do professor Rogério: https://github.com/rogercodeeti
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return '<h1>Home</h1>'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
